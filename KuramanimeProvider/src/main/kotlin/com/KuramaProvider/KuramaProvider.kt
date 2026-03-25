3030a371760f2beed5447d5ab93e6c6de5e038bb
Anya <alimuzakky24@gmail.com>
2026-03-25 14:34:16 +0700

    .

--- KuramanimeProvider/src/main/kotlin/com/KuramaProvider/KuramaProvider.kt
+++ KuramanimeProvider/src/main/kotlin/com/KuramaProvider/KuramaProvider.kt
@@ -8,7 +8,7 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
 class KuramanimeProvider : MainAPI() {
-    override var mainUrl = "https://kuramanime.pro"
+    override var mainUrl = "https://v17.kuramanime.ink/"
     override var name = "Kuramanime"
     override val hasQuickSearch = false
     override val hasMainPage = true
