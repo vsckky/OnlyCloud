237d8b8d291c2cde6bb3d040971e77ac26250028
Anya <alimuzakky24@gmail.com>
2026-03-25 12:35:48 +0700

    Build 82c9f2ea2de3ecf3343e4c46a7bcc087424cfb43

---
+++ AnichinMoe/build.gradle.kts
@@ -0,0 +1,17 @@
+version = 25
+
+cloudstream {
+    description = "Anichin Moe"
+    
+    language = "id"
+    
+    authors = listOf("vsc")
+    
+    status = 1
+    
+    tvTypes = listOf("AnimeMovie", "Anime", "Cartoon")
+    
+    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://anichin.club&size=%size%"
+    
+    isCrossPlatform = false
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+vsc : 
+ https://raw.githubusercontent.com/vsckky/OnlyCloud/main/repo.json
