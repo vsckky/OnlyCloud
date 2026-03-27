68a6d60392381a05a0dbc15a064ead4ff01ab99a
Anya <alimuzakky24@gmail.com>
2026-03-27 13:55:35 +0700

    .

---
+++ Nekopoi/src/main/kotlin/com/nekopoi/Extractors.kt
@@ -0,0 +1,32 @@
+package com.nekopoi
+
+import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.newExtractorLink
+
+open class ZippyShare : ExtractorApi() {
+    override val name = "ZippyShare"
+    override val mainUrl = "https://zippysha.re"
+    override val requiresReferer = true
+
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        val res = app.get(url, referer = referer).document
+        val video = res.selectFirst("a#download-url")?.attr("href")
+        callback.invoke(
+			newExtractorLink(
+                name,
+                name,
+                video ?: return
+            ){
+				this.referer = "$mainUrl/"
+			}
+        )
+    }
+}
