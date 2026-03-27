0991f32c8c45ed9c0920a575ec322f9b85ed869a
Anya <alimuzakky24@gmail.com>
2026-03-27 17:38:54 +0700

    //

--- JavHD/build.gradle.kts
+++ JavHD/build.gradle.kts
@@ -1,12 +1,12 @@
 // use an integer for version numbers
-version = 8
+//version = 8
 
 
-cloudstream {
+//cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    description = "HD JAV Library"
-    authors = listOf("vsc")
+//    description = "HD JAV Library"
+//    authors = listOf("vsc")
 
     /**
     * Status int as the following:
@@ -15,14 +15,14 @@ cloudstream {
     * 2: Slow
     * 3: Beta only
     * */
-    status = 1 // will be 3 if unspecified
+//    status = 1 // will be 3 if unspecified
 
     // List of video source types. Users are able to filter for extensions in a given category.
     // You can find a list of avaliable types here:
     // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
     tvTypes = listOf("NSFW")
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=javhd.icu&sz=%size%"
+//    iconUrl = "https://www.google.com/s2/favicons?domain=javhd.icu&sz=%size%"
 
-    language = "id"
+//    language = "id"
 }
