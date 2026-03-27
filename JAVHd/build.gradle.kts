01812e5e73bb008dc63dc7d841e749bd5a8baf4b
Anya <alimuzakky24@gmail.com>
2026-03-28 00:14:31 +0700

    //

---
+++ JAVHd/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 5
+
+cloudstream {
+    authors     = listOf("vsc")
+    language    = "id"
+//    description = "The Best Free JAV online, Free Download and Watching jav porn videos. Tons of hot jav censored,Japanese tube,Japanese sex online. Update everyday, javhd.today are waiting for you. Enjoy!"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://javhd.today/logo/javhd1.png"
+}
