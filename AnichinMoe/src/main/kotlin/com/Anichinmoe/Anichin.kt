40536c97ccde3b59e8701b57ca09a4d3123ce2b3
Anya <alimuzakky24@gmail.com>
2026-03-27 18:36:52 +0700

    //

--- AnichinMoe/src/main/kotlin/com/Anichinmoe/Anichin.kt
+++ AnichinMoe/src/main/kotlin/com/Anichinmoe/Anichin.kt
@@ -10,7 +10,7 @@ class Anichin : MainAPI() {
         var context: android.content.Context? = null
     }
     override var mainUrl = "https://anichin.moe"
-    override var name = "Anichin"
+    override var name = "Anichin🌐"
     override val hasMainPage = true
     override var lang = "id"
     override val hasDownloadSupport = true
