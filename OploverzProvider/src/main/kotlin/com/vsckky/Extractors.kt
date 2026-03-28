f364902d6d749eb4d9ea1bfe86bf47d8127756f8
Anya <alimuzakky24@gmail.com>
2026-03-29 02:34:31 +0700

    //

--- OploverzProvider/src/main/kotlin/com/vsckky/Extractors.kt
+++ OploverzProvider/src/main/kotlin/com/vsckky/Extractors.kt
@@ -2,6 +2,7 @@ package com.vsckky
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 
