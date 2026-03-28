fe536c94b029073c10b007bac4fc540762f53f90
Anya <alimuzakky24@gmail.com>
2026-03-29 01:32:49 +0700

    //

---
+++ OploverzProvider/src/main/kotlin/com/vsckky/OploverzProviderPlugin.kt
@@ -0,0 +1,19 @@
+
+package com.vsckky
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class OploverzProviderPlugin: Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        registerMainAPI(OploverzProvider())
+        registerExtractorAPI(Qiwi())
+        registerExtractorAPI(Filedon())
+        registerExtractorAPI(Buzzheavier())
+        registerExtractorAPI(Doply())
+        registerExtractorAPI(Mp4Upload2())
+    }
+}
