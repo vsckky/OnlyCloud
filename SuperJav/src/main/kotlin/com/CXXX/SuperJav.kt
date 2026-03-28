b94248d661fe7bb186d9302ee925d8bb71f12e5d
Anya <alimuzakky24@gmail.com>
2026-03-28 16:17:45 +0700

    //

--- SuperJav/src/main/kotlin/com/CXXX/SuperJav.kt
+++ SuperJav/src/main/kotlin/com/CXXX/SuperJav.kt
@@ -24,6 +24,7 @@ class SuperJav : MainAPI() {
         "popular?sort=month" to "Populer Bulan Ini",
         "tag/mother" to "Ibu",
         "tag/stepmother" to "Ibu Tiri",
+        "tag/adopted-daughter" to "Anak Angkat",
         "tag/tits" to "Payudara",
         "category/english-subtitles" to "Subtitel Inggris",
         "tag/4k" to "4K",
