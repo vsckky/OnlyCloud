144c3c30c13565871a52456e70b3579f46d33a0a
Anya <alimuzakky24@gmail.com>
2026-03-29 12:03:16 +0700

    //

--- OploverzProvider/src/main/kotlin/com/vsckky/OploverzProviderPlugin.kt
+++ OploverzProvider/src/main/kotlin/com/vsckky/OploverzProviderPlugin.kt
@@ -10,9 +10,9 @@ class OploverzProviderPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(OploverzProvider())
+        registerExtractorAPI(Box())
         registerExtractorAPI(Qiwi())
         registerExtractorAPI(Filedon())
         registerExtractorAPI(Buzzheavier())
-        registerExtractorAPI(Box())
     }
 }
