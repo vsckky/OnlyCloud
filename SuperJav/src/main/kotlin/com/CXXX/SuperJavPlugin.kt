f86d8173a1194c7f93c51c4a5dde813bf0091cdb
Anya <alimuzakky24@gmail.com>
2026-03-30 12:26:56 +0700

    //

--- SuperJav/src/main/kotlin/com/CXXX/SuperJavPlugin.kt
+++ SuperJav/src/main/kotlin/com/CXXX/SuperJavPlugin.kt
@@ -11,7 +11,7 @@ class SuperJavPlugin: Plugin() {
         registerMainAPI(SuperJav())
         registerExtractorAPI(EmturbovidExtractor())
         registerExtractorAPI(StreamTape())
-        registerExtractorAPI(watchadsontape())
+        registerExtractorAPI(altaq())
         registerExtractorAPI(StreamHG())
     }
 }
