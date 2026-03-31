83fdc6fe05032788143be7f1b80a130f7ec0bc0f
Anya <alimuzakky24@gmail.com>
2026-03-31 15:15:31 +0700

    //

---
+++ FreeReels/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 2
+
+cloudstream {
+    description = ""
+    language = "id"
+    authors = listOf("vsc")
+
+    status = 1
+    tvTypes = listOf(
+        "TvSeries",
+        "AsianDrama",
+    )
+
+    iconUrl =
+        "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://free-reels.com&size=%size%"
+}
